76869ee607d331ac94e86b37497729301eb885b2
cbpark <cbpark@gmail.com>
2014-06-17 00:05:27 +0200

    Add a function for generating histogram

--- exe/EratioTrue.hs
+++ exe/EratioTrue.hs
@@ -1,12 +1,14 @@
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import           Options.Applicative
+import           Data.Maybe
+import qualified Data.Vector                 as V
+import qualified Data.Vector.Generic         as G
 import           Database.HDBC
-import           Database.HDBC.Sqlite3           (connectSqlite3)
-import Data.Maybe
+import           Database.HDBC.Sqlite3       (connectSqlite3)
+import           Options.Applicative
+import           Statistics.Sample.Histogram (histogram_)
 
 data Args = Args { input :: [String], output :: String }
 
@@ -14,23 +16,31 @@ cmdoptions :: Parser Args
 cmdoptions = Args <$> some (argument str ( metavar "INPUTS"
                                         <> help "Input DB files")) <*>
                       strOption ( long "output"
-                               <> metavar "DATA"
+                               <> short 'o'
+                               <> metavar "OUTPUT"
                                <> help "Output data file to save the result" )
 
 mkHist :: Args -> IO ()
 mkHist (Args infiles outfile) = do
   vss <- mapM (query "er_true" "mbl_true > 0") infiles
-  mapM_ print vss
+  let hists = map (mkHist' 100 0 1) vss
+  mapM_ print hists
   putStrLn $ "Output file: " ++ outfile
 
-query :: String -> String -> FilePath -> IO [Double]
+mkHist' :: Int -> Double -> Double -> V.Vector Double -> V.Vector (Double, Double)
+mkHist' nbin lower upper xs = V.zip bins hist
+    where bins = G.generate nbin step
+          step i = lower + d * fromIntegral i
+          d = (upper - lower) / fromIntegral nbin
+          hist = histogram_ nbin lower upper xs
+
+query :: String -> String -> FilePath -> IO (V.Vector Double)
 query var cut infile = do
   conn <- connectSqlite3 infile
   v <- quickQuery' conn ("SELECT " ++ var ++ " from var where " ++ cut) []
   let rows = map convRow v
   disconnect conn
-  return $ map (fromMaybe (-10)) rows
-
+  return . V.fromList $ map (fromMaybe (-10)) rows
       where convRow :: [SqlValue] -> Maybe Double
             convRow [sqlVal] = (return . fromSql) sqlVal
             convRow _        = Nothing
