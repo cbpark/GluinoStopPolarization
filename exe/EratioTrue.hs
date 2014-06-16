cf1da5e19a7f1e4525f5914bc60930b5394d29bd
cbpark <cbpark@gmail.com>
2014-06-16 23:12:08 +0200

    Add db query function

--- exe/EratioTrue.hs
+++ exe/EratioTrue.hs
@@ -4,6 +4,9 @@
 module Main where
 
 import           Options.Applicative
+import           Database.HDBC
+import           Database.HDBC.Sqlite3           (connectSqlite3)
+import Data.Maybe
 
 data Args = Args { input :: [String], output :: String }
 
@@ -16,8 +19,21 @@ cmdoptions = Args <$> some (argument str ( metavar "INPUTS"
 
 mkHist :: Args -> IO ()
 mkHist (Args infiles outfile) = do
-  mapM_ putStrLn infiles
+  vss <- mapM (query "er_true" "mbl_true > 0") infiles
+  mapM_ print vss
   putStrLn $ "Output file: " ++ outfile
+
+query :: String -> String -> FilePath -> IO [Double]
+query var cut infile = do
+  conn <- connectSqlite3 infile
+  v <- quickQuery' conn ("SELECT " ++ var ++ " from var where " ++ cut) []
+  let rows = map convRow v
+  disconnect conn
+  return $ map (fromMaybe (-10)) rows
+
+      where convRow :: [SqlValue] -> Maybe Double
+            convRow [sqlVal] = (return . fromSql) sqlVal
+            convRow _        = Nothing
 
 main :: IO ()
 main =
